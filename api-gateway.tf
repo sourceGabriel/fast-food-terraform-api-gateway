551f4d92c207c92cbf78e6610273ef1e4dfb56e3
Rafael Moura <rafael.mesquita.moura@gmail.com>
2024-09-06 00:32:52 -0300

    feat: Seggregate AWS Lambda Function terraformation

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,16 +1,16 @@
-resource "aws_apigatewayv2_api" "lambda" {
-  name          = "serverless_lambda_gw"
+resource "aws_apigatewayv2_api" "api-gateway" {
+  name          = "fiap-fast-food-api-gateway"
   protocol_type = "HTTP"
 }
 
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.lambda.id
+resource "aws_apigatewayv2_stage" "api-gateway" {
+  api_id = aws_apigatewayv2_api.api-gateway.id
 
-  name        = "serverless_lambda_stage"
+  name        = "fiap-fast-food-api-gateway-stage"
   auto_deploy = true
 
   access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.api_gw.arn
+    destination_arn = aws_cloudwatch_log_group.api-gateway.arn
 
     format = jsonencode({
       requestId               = "$context.requestId"
@@ -27,32 +27,8 @@ resource "aws_apigatewayv2_stage" "lambda" {
   }
 }
 
-resource "aws_apigatewayv2_integration" "login" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  integration_uri    = aws_lambda_function.login.invoke_arn
-  integration_type   = "AWS_PROXY"
-  integration_method = "POST"
-}
-
-resource "aws_apigatewayv2_route" "login" {
-  api_id = aws_apigatewayv2_api.lambda.id
-
-  route_key = "GET /clientes"
-  target    = "integrations/${aws_apigatewayv2_integration.login.id}"
-}
-
-resource "aws_cloudwatch_log_group" "api_gw" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.lambda.name}"
+resource "aws_cloudwatch_log_group" "api-gateway" {
+  name = "/aws/api-gateway/${aws_apigatewayv2_api.api-gateway.name}"
 
   retention_in_days = 30
-}
-
-resource "aws_lambda_permission" "api_gw" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.login.function_name
-  principal     = "apigateway.amazonaws.com"
-
-  source_arn = "${aws_apigatewayv2_api.lambda.execution_arn}/*/*"
 }

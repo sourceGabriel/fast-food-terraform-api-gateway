c44d56d87ae4565d91e5a42f51ecefd93417d80a
Rafael Moura <rafael.mesquita.moura@gmail.com>
2024-09-12 22:58:15 -0300

    feat: Create AWS API Gateway REST

--- api-gateway.tf
+++ api-gateway.tf
@@ -1,4 +1,8 @@
-resource "aws_apigatewayv2_api" "api-gateway" {
-  name          = "fiap-fast-food-api-gateway"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "api-gateway" {
+  name        = "fiap-fast-food-api-gateway"
+  description = "Main AWS API Gateway to Tech Challenge's phase 3"
+}
+
+provider "aws" {
+  # Configuration options
 }
